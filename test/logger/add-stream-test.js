512c3af8f393ee7a3ca6bd32e1d6ab67a23189a9
Kris Walker <kris@kixx.name>
2018-11-25 07:11:37 -0500

    Prevent adding a stream multiple times.

--- test/logger/add-stream-test.js
+++ test/logger/add-stream-test.js
@@ -179,5 +179,58 @@ module.exports = (t) => {
 				assert.isEqual(Logger.FATAL, call2.args[0].level);
 			});
 		});
+
+		t1.describe('with already attached stream', (t2) => {
+			const sandbox = Sinon.createSandbox();
+
+			let subject;
+
+			const stream = {
+				write() {}
+			};
+
+			t2.before((done) => {
+				sandbox.stub(stream, 'write');
+
+				subject = Logger.create({
+					level: Logger.TRACE,
+					stream
+				});
+
+				subject.addStream(stream);
+
+				subject.trace('trace message');
+				subject.debug('debug message');
+				subject.info('info message');
+				subject.warn('warn message');
+				subject.error('error message');
+				subject.fatal('fatal message');
+
+				done();
+			});
+
+			t2.after((done) => {
+				sandbox.restore();
+				done();
+			});
+
+			t2.it('calls every log level once', () => {
+				assert.isEqual(6, stream.write.callCount);
+
+				const call1 = stream.write.getCall(0);
+				const call2 = stream.write.getCall(1);
+				const call3 = stream.write.getCall(2);
+				const call4 = stream.write.getCall(3);
+				const call5 = stream.write.getCall(4);
+				const call6 = stream.write.getCall(5);
+
+				assert.isEqual(Logger.TRACE, call1.args[0].level);
+				assert.isEqual(Logger.DEBUG, call2.args[0].level);
+				assert.isEqual(Logger.INFO, call3.args[0].level);
+				assert.isEqual(Logger.WARN, call4.args[0].level);
+				assert.isEqual(Logger.ERROR, call5.args[0].level);
+				assert.isEqual(Logger.FATAL, call6.args[0].level);
+			});
+		});
 	});
 };
