15029a0912b1b8d57dc41375e242acc8bbb0ad47
Kris Walker <kris@kixx.name>
2018-11-25 06:31:21 -0500

    Testing complete for Logger#addStream()

--- test/logger/add-stream-test.js
+++ test/logger/add-stream-test.js
@@ -58,13 +58,126 @@ module.exports = (t) => {
 			});
 		});
 
-		t1.xdescribe('with init() function', (t2) => {
+		t1.describe('with init() function', (t2) => {
+			const sandbox = Sinon.createSandbox();
+
+			let subject;
+
+			const stream = {
+				init() {}
+			};
+
+			t2.before((done) => {
+				sandbox.stub(stream, 'init');
+
+				subject = Logger.create();
+
+				subject.addStream(stream);
+
+				done();
+			});
+
+			t2.after((done) => {
+				sandbox.restore();
+				done();
+			});
+
+			t2.it('calls the init function', () => {
+				assert.isEqual(1, stream.init.callCount);
+				assert.isEqual(0, stream.init.firstCall.args.length);
+			});
 		});
 
-		t1.xdescribe('with explicit level', (t2) => {
+		t1.describe('with explicit level', (t2) => {
+			const sandbox = Sinon.createSandbox();
+
+			let subject;
+
+			const stream = {
+				level: Logger.ERROR,
+				write() {}
+			};
+
+			t2.before((done) => {
+				sandbox.stub(stream, 'write');
+
+				subject = Logger.create({
+					level: Logger.TRACE
+				});
+
+				subject.addStream(stream);
+
+				subject.trace('trace message');
+				subject.debug('debug message');
+				subject.info('info message');
+				subject.warn('warn message');
+				subject.error('error message');
+				subject.fatal('fatal message');
+
+				done();
+			});
+
+			t2.after((done) => {
+				sandbox.restore();
+				done();
+			});
+
+			t2.it('calls set log level', () => {
+				assert.isEqual(2, stream.write.callCount);
+
+				const call1 = stream.write.getCall(0);
+				const call2 = stream.write.getCall(1);
+
+				assert.isEqual(Logger.ERROR, call1.args[0].level);
+				assert.isEqual(Logger.FATAL, call2.args[0].level);
+			});
 		});
 
-		t1.xdescribe('with children', (t2) => {
+		t1.describe('with children', (t2) => {
+			const sandbox = Sinon.createSandbox();
+
+			let subject;
+
+			const stream = {
+				level: Logger.ERROR,
+				write() {}
+			};
+
+			t2.before((done) => {
+				sandbox.stub(stream, 'write');
+
+				const parent = Logger.create({
+					level: Logger.TRACE
+				});
+
+				subject = parent.create('My Logger');
+
+				parent.addStream(stream);
+
+				subject.trace('trace message');
+				subject.debug('debug message');
+				subject.info('info message');
+				subject.warn('warn message');
+				subject.error('error message');
+				subject.fatal('fatal message');
+
+				done();
+			});
+
+			t2.after((done) => {
+				sandbox.restore();
+				done();
+			});
+
+			t2.it('calls set log level', () => {
+				assert.isEqual(2, stream.write.callCount);
+
+				const call1 = stream.write.getCall(0);
+				const call2 = stream.write.getCall(1);
+
+				assert.isEqual(Logger.ERROR, call1.args[0].level);
+				assert.isEqual(Logger.FATAL, call2.args[0].level);
+			});
 		});
 	});
 };
