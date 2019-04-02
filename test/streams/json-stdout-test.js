5f14c370f3ea1c986d50e4843da110bf132057c0
Kris Walker <kris@kixx.name>
2019-04-02 06:59:53 -0400

    Update JSON stdout test.

--- test/streams/json-stdout-test.js
+++ test/streams/json-stdout-test.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const { assert } = require('kixx-assert');
-const Sinon = require('sinon');
+const sinon = require('sinon');
 const { Logger, streams } = require('../../');
-const { EOL } = require('os');
+const os = require('os');
+
+const { EOL } = os;
 
 function delay(ms) {
 	return new Promise((resolve) => {
@@ -13,107 +15,114 @@ function delay(ms) {
 	});
 }
 
+function noop() {
+}
+
+const DEFAULT_NAME = 'root';
+
 module.exports = (t) => {
-	t.describe('JsonStdoutStream with defaults', (t1) => {
-		const sandbox = Sinon.createSandbox();
+	t.describe('JSONStream with defaults', (t1) => {
+		const hostname = os.hostname();
+		const pid = process.pid;
 
 		t1.before((done) => {
-			sandbox.stub(process.stdout, 'write').callsFake(() => {});
+			sinon.stub(process.stdout, 'write').callsFake(noop);
 
 			const logger = Logger.create({
-				stream: streams.JsonStdout.create()
+				streams: [ streams.JSONStream.create() ]
+			});
+
+			logger.trace('default fields');
+			logger.trace('additional fields', {
+				foo: [ { bar: 'baz' }, 1, false ]
+			});
+
+			logger.debug('default fields');
+			logger.debug('additional fields', {
+				foo: [ { bar: 'baz' }, 1, false ]
 			});
 
 			logger.info('default fields');
-
 			logger.info('additional fields', {
 				foo: [ { bar: 'baz' }, 1, false ]
 			});
 
-			// Introduce some delay to catch the async call.
-			return delay(10).then(done);
-		});
-
-		t1.after((done) => {
-			sandbox.restore();
-			done();
-		});
-
-		t1.it('writes the expected number of times', () => {
-			assert.isEqual(2, process.stdout.write.callCount);
-		});
-
-		t1.it('writes expected output string for default fields', () => {
-			const { args } = process.stdout.write.firstCall;
-			const { hostname, pid, time } = JSON.parse(args[0]);
-			assert.isEqual(`{"name":"root","hostname":"${hostname}","pid":${pid},"time":"${time}","level":30,"msg":"default fields"}${EOL}`, args[0]);
-		});
-
-		t1.it('writes expected output string for additional fields', () => {
-			const { args } = process.stdout.write.secondCall;
-			const { hostname, pid, time } = JSON.parse(args[0]);
-			assert.isEqual(`{"name":"root","hostname":"${hostname}","pid":${pid},"time":"${time}","level":30,"msg":"additional fields","foo":[{"bar":"baz"},1,false]}${EOL}`, args[0]);
-		});
-	});
-
-	t.describe('JsonStdoutStream with makePretty=true', (t1) => {
-		const sandbox = Sinon.createSandbox();
-
-		t1.before((done) => {
-			sandbox.stub(process.stdout, 'write').callsFake(() => {});
-
-			const logger = Logger.create({
-				stream: streams.JsonStdout.create({
-					makePretty: true
-				})
+			logger.warn('default fields');
+			logger.warn('additional fields', {
+				foo: [ { bar: 'baz' }, 1, false ]
 			});
 
-			logger.info('default fields');
+			logger.error('default fields');
+			logger.error('additional fields', {
+				foo: [ { bar: 'baz' }, 1, false ]
+			});
 
-			logger.info('additional fields', {
+			logger.fatal('default fields');
+			logger.fatal('additional fields', {
 				foo: [ { bar: 'baz' }, 1, false ]
 			});
 
 			// Introduce some delay to catch the async call.
-			return delay(10).then(done);
+			return delay(60).then(done);
 		});
 
 		t1.after((done) => {
-			sandbox.restore();
+			sinon.restore();
 			done();
 		});
 
 		t1.it('writes the expected number of times', () => {
-			assert.isEqual(2, process.stdout.write.callCount);
+			assert.isEqual(12, process.stdout.write.callCount);
 		});
 
-		t1.it('writes expected output string for default fields', () => {
-			const { args } = process.stdout.write.firstCall;
-			assert.isMatch(/^[\d]{4}-[\d]{2}-[\d]{2}T[\d]{2}:[\d]{2}:[\d]{2}.[\d]{3}Z\s-\sINFO\s\(30\)\s-\sroot\s-\sdefault\sfields$/, args[0].trim());
-			assert.isOk(args[0].endsWith(EOL));
-		});
+		t1.it('writes expected output strings', () => {
+			const args = process.stdout.write.getCalls().map((call) => call.args[0]);
 
-		t1.it('writes expected output string for additional fields', () => {
-			const { args } = process.stdout.write.secondCall;
-			assert.isOk(args[0].endsWith(EOL));
-			const lines = args[0].split(EOL).filter((x) => Boolean(x));
+			const levels = [
+				Logger.Levels.TRACE,
+				Logger.Levels.DEBUG,
+				Logger.Levels.INFO,
+				Logger.Levels.WARN,
+				Logger.Levels.ERROR,
+				Logger.Levels.FATAL,
+			];
 
-			assert.isEqual(2, lines.length);
+			assert.isEqual(12, args.length);
 
-			assert.isMatch(/^[\d]{4}-[\d]{2}-[\d]{2}T[\d]{2}:[\d]{2}:[\d]{2}.[\d]{3}Z\s-\sINFO\s\(30\)\s-\sroot\s-\sadditional\sfields\s-$/, lines[0]);
+			args.forEach((str, i) => {
+				assert.isEqual('string', typeof str);
+				assert.isOk(str.endsWith(EOL));
+				const record = JSON.parse(str.trim());
 
-			assert.isEqual("{ foo: [ { bar: 'baz' }, 1, false ] }", lines[1]); // eslint-disable-line quotes
+				assert.isEqual(DEFAULT_NAME, record.name);
+				assert.isEqual(hostname, record.hostname);
+				assert.isEqual(pid, record.pid);
+
+				const time = new Date(record.time);
+				const now = new Date();
+				assert.isEqual(now.getDate(), time.getDate());
+
+				const level = levels[Math.ceil(i / 2)];
+				assert.isEqual(level, record.level);
+
+				if (i % 2 === 1) {
+					assert.isEqual('additional fields', record.msg);
+					assert.isEqual('baz', record.foo[0].bar);
+					assert.isEqual(1, record.foo[1]);
+					assert.isEqual(false, record.foo[2]);
+				} else {
+					assert.isEqual('default fields', record.msg);
+				}
+			});
 		});
 	});
 
-	t.describe('JsonStdoutStream with circular reference', (t1) => {
-		const sandbox = Sinon.createSandbox();
-
+	t.describe('JSONStream with circular reference', (t1) => {
 		t1.before((done) => {
-			sandbox.stub(process.stdout, 'write').callsFake(() => {});
+			sinon.stub(process.stdout, 'write').callsFake(noop);
 
 			const logger = Logger.create({
-				stream: streams.JsonStdout.create()
+				streams: [ streams.JSONStream.create() ]
 			});
 
 			const myObject = {
@@ -130,7 +139,7 @@ module.exports = (t) => {
 		});
 
 		t1.after((done) => {
-			sandbox.restore();
+			sinon.restore();
 			done();
 		});
 
@@ -141,57 +150,9 @@ module.exports = (t) => {
 		t1.it('writes expected output', () => {
 			const { args } = process.stdout.write.firstCall;
 			assert.isOk(args[0].endsWith(EOL));
-			const { hostname, pid, time } = JSON.parse(args[0]);
-			assert.isEqual(`{"name":"root","hostname":"${hostname}","pid":${pid},"time":"${time}","level":30,"msg":"circular fields","foo":"bar","bar":"baz","circ":{"foo":"bar","bar":"baz","circ":"[Circular]"}}${EOL}`, args[0]);
-		});
-	});
-
-	t.describe('JsonStdoutStream with circular reference and makePretty', (t1) => {
-		const sandbox = Sinon.createSandbox();
-
-		t1.before((done) => {
-			sandbox.stub(process.stdout, 'write').callsFake(() => {});
-
-			const logger = Logger.create({
-				stream: streams.JsonStdout.create({
-					makePretty: true
-				})
-			});
-
-			const myObject = {
-				foo: 'bar',
-				bar: 'baz'
-			};
-
-			myObject.circ = myObject;
-
-			logger.info('circular fields', myObject);
-
-			// Introduce some delay to catch the async call.
-			return delay(10).then(done);
-		});
-
-		t1.after((done) => {
-			sandbox.restore();
-			done();
-		});
-
-		t1.it('writes the expected number of times', () => {
-			assert.isEqual(1, process.stdout.write.callCount);
-		});
-
-		t1.it('writes expected output', () => {
-			const { args } = process.stdout.write.firstCall;
-			assert.isOk(args[0].endsWith(EOL));
-			const lines = args[0].split(EOL).filter((x) => Boolean(x));
-
-			assert.isEqual(4, lines.length);
-
-			assert.isMatch(/^[\d]{4}-[\d]{2}-[\d]{2}T[\d]{2}:[\d]{2}:[\d]{2}.[\d]{3}Z\s-\sINFO\s\(30\)\s-\sroot\s-\scircular\sfields\s-$/, lines[0]);
-
-			assert.isEqual("{ foo: 'bar',", lines[1]); // eslint-disable-line quotes
-			assert.isEqual("  bar: 'baz',", lines[2]); // eslint-disable-line quotes
-			assert.isEqual("  circ: { foo: 'bar', bar: 'baz', circ: [Circular] } }", lines[3]); // eslint-disable-line quotes
+			const { hostname, pid, time } = JSON.parse(args[0].trim());
+			const firstPart = `{"name":"${DEFAULT_NAME}","hostname":"${hostname}","pid":${pid},"time":"${time}","level":30,"msg":"circular fields"`;
+			assert.isEqual(`${firstPart},"foo":"bar","bar":"baz","circ":{"foo":"bar","bar":"baz","circ":"[Circular]"}}${EOL}`, args[0]);
 		});
 	});
 };
