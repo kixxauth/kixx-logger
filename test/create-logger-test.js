d58820218eb9ed64df1a07a87e0aecee4ed4ff9d
Kris Walker <kris@kixx.name>
2019-04-04 05:43:30 -0400

    Moving tests around.

--- test/create-logger-test.js
+++ test/create-logger-test.js
@@ -341,42 +341,28 @@ module.exports = (t) => {
 
 	t.describe('with options.serializers', (t1) => {
 		let logger;
-		let stream;
-		const foo = 'bar';
-		const baz = { a: 'z' };
-		const err = {};
+
+		const thisSerializers = {
+			foo() {
+				return identity;
+			},
+			baz() {
+				return identity;
+			}
+		};
 
 		t1.before((done) => {
+			Object.keys(serializers).forEach((key) => {
+				sinon.spy(serializers, key);
+			});
+
+			Object.keys(thisSerializers).forEach((key) => {
+				sinon.spy(thisSerializers, key);
+			});
+
 			logger = createLogger({
-				serializers: {
-					foo() {
-						return sinon.spy(identity);
-					},
-					baz() {
-						return sinon.spy(identity);
-					},
-					err() {
-						return sinon.spy(identity);
-					}
-				}
+				serializers: thisSerializers
 			});
-
-			assert.isEqual(logger.streams.length, 1);
-
-			stream = logger.streams[0];
-
-			sinon.stub(stream, '_transform').callsFake(noop);
-
-			Object.keys(stream.serializers).forEach((key) => {
-				sinon.stub(stream.serializers, key).callsFake(identity);
-			});
-
-			logger.trace('trace level message');
-			logger.debug('default fields');
-			logger.info('with extra properties', { foo, baz });
-			logger.warn('yellow and orange');
-			logger.error('with an error', { err });
-			logger.fatal('i quit');
 
 			done();
 		});
@@ -411,17 +397,33 @@ module.exports = (t) => {
 			assert.isEqual(stream, thisStream);
 		});
 
+		t1.it('has default serializer', () => {
+			assert.isEqual(3, Object.keys(logger.serializers).length);
+			assert.isEqual('function', typeof logger.serializers.err);
+
+			assert.isEqual(1, serializers.err.callCount);
+
+			const options = serializers.foo.getCall(0).args[0];
+			assert.isEqual('object', typeof options);
+			assert.isNotEmpty(options);
+		});
+
 		t1.it('has given serializers', () => {
-			assert.isEqual(3, Object.keys(stream.serializers).length);
-			assert.isEqual('function', typeof stream.serializers.foo);
-			assert.isEqual('function', typeof stream.serializers.baz);
-			assert.isEqual('function', typeof stream.serializers.err);
-			assert.isEqual(1, stream.serializers.foo.callCount);
-			assert.isEqual(1, stream.serializers.baz.callCount);
-			assert.isEqual(1, stream.serializers.err.callCount);
-			assert.isOk(stream.serializers.foo.calledWith(foo));
-			assert.isOk(stream.serializers.baz.calledWith(baz));
-			assert.isOk(stream.serializers.err.calledWith(err));
+			assert.isEqual(3, Object.keys(logger.serializers).length);
+			assert.isEqual('function', typeof logger.serializers.foo);
+			assert.isEqual('function', typeof logger.serializers.baz);
+			assert.isEqual(1, serializers.foo.callCount);
+			assert.isEqual(1, serializers.baz.callCount);
+
+			let options;
+
+			options = serializers.foo.getCall(0).args[0];
+			assert.isEqual('object', typeof options);
+			assert.isNotEmpty(options);
+
+			options = serializers.baz.getCall(0).args[0];
+			assert.isEqual('object', typeof options);
+			assert.isNotEmpty(options);
 		});
 	});
 
